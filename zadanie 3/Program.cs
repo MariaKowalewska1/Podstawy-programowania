735493730bb6c4a4371ea0b7dd82d18286604cd9
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-03-03 16:36:03 +0100

    koniec zajęć - zadania 1,2,3,4

---
+++ zadanie 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Napisać program wczytujący z klawiatury liczbę całkowitą i wypisujący na ekranie sześcian wczytanej liczby.
+            int a;
+            Console.Write("Podaj liczbe calkowita: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a^3={0} ", a*a*a);
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
